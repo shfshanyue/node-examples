2c4875cf0159530fd75331018d90d61e29d83026
shfshanyue <xianger94@gmail.com>
2021-08-26 15:51:42 +0800

    chore: update express

--- server/express/hello.js
+++ server/express/hello.js
@@ -1,8 +1,34 @@
 const express = require('express')
 const app = express()
 
-app.get('/api', (req, res, next) => {
-  next()
+app.use('/api',
+  (req, res, next) => {
+    // 应用中间件 A
+    console.log('Application Level Middleware: A')
+    next()
+  },
+  (req, res, next) => {
+    // 应用中间件 B
+    console.log('Application Level Middleware: B')
+    next()
+  }
+)
+
+app.get('/api',
+  (req, res, next) => {
+    // 路由中间件 A
+    console.log('Route Level Middleware: C')
+    next()
+  },
+  (req, res, next) => {
+    // 路由中间件 A
+    console.log('Route Level Middleware: D')
+    res.json({ hello: 'world' })
+  }
+)
+
+app.get('/api/users/:id', (req, res) => {
+  res.json({ hello: 'world', id: req.params.id })
 })
 
 app.listen(3000);
