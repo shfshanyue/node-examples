06336ea826ae8e9ca7c13a3f7be892502632725f
shfshanyue <xianger94@gmail.com>
2021-08-31 18:29:02 +0800

    chore: update stream code

--- native/net/index.js
+++ native/net/index.js
@@ -2,29 +2,34 @@ const net = require('net')
 
 // 创建一个 TCP socket
 const server = net.createServer((socket) => {
-  socket.end('echo\n')
+  socket.on('data', data => {
+    console.log(data)
+  })
+  socket.end('echo\r\n')
 })
 
 server.listen(8080, () => {
-  console.log('opened server on', server.address());
+  console.log('Server 8080: ', server.address());
 });
 
 setTimeout(() => {
+  // net client 实现
+  // 为了使 Server 启动完成，在 1s 之后，client 再去试图连接 server
   const client = net.createConnection({
     timeout: 3000,
     port: 8080,
     // allowHalfOpen: true
   }, () => {
-    console.log('connected to server!')
+    console.log(`Client ${client.localPort}: 该客户端已与服务端取得联系`)
     client.write('hello\r\n')
   })
 
   client.on('data', (data) => {
-    console.log(data.toString())
+    console.log(`Client ${client.localPort} Received: ${data}`)
     client.end()
   })
 
   client.on('end', () => {
-    console.log('disconnected from server')
+    console.log(`Client ${client.localPort} : 该客户端已下线`)
   })
-}, 3000)
+}, 1000)
--- native/stream/write.js
+++ native/stream/write.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const sleep = s => new Promise(res => setTimeout(res, s))
 
 // interface StreamOptions {
 //   flags?: string;
@@ -11,17 +12,69 @@ const fs = require('fs')
 //   highWaterMark?: number;
 // }
 
-const stream = fs.createWriteStream('./example.jsonl', {
-  highWaterMark: 3
-})
+async function main () {
+  console.log('示例一:')
+  {
+    const stream = fs.createWriteStream('./example-w.jsonl')
 
-stream.on('finish', e => console.log('FINISH'))
+    stream.on('finish', e => console.log('FINISH'))
+    stream.on('close', e => console.log('CLOSE'))
+    stream.on('drain', e => console.log('DRAIN'))
 
-console.log(stream.write('{"name": "shanyue"}\n', 'utf8'), () => console.log('FLUSH...'))
-console.log(stream.write('{"name": "shanyue"}\n'))
-console.log(stream.write('{"name": "shanyue"}\n'))
-console.log(stream.write('{"name": "shanyue"}\n'))
-console.log(stream.write('{"name": "shanyue"}\n'))
-stream.end()
+    console.log(stream.write('{"name": "shanyue"}\n', 'utf8', () => console.log('FLUSH...')))
+    console.log(stream.write('{"name": "shanyue"}\n'))
+    console.log(stream.write('{"name": "shanyue"}\n'))
+    console.log(stream.write('{"name": "shanyue"}\n'))
+    console.log(stream.write('{"name": "shanyue"}\n'))
+    stream.end()
+  }
 
-console.log(stream)
+  await sleep(1000)
+
+  // 示例二:
+  console.log('\n\n示例二:')
+  {
+    const stream = fs.createWriteStream('./example-w.jsonl', { highWaterMark: 10 })
+
+    stream.on('finish', e => console.log('FINISH'))
+    stream.on('close', e => console.log('CLOSE'))
+    stream.on('drain', e => console.log('DRAIN'))
+
+    console.log(stream.write('{"name": "shanyue"}\n', 'utf8', () => console.log('FLUSH...')))
+    console.log(stream.write('{"name": "shanyue"}\n'))
+    console.log(stream.write('{"name": "shanyue"}\n'))
+    console.log(stream.write('{"name": "shanyue"}\n'))
+    console.log(stream.write('{"name": "shanyue"}\n'))
+    stream.end()
+  }
+
+  await sleep(1000)
+
+  console.log('示例三')
+  {
+    const Stream = require('stream');
+
+    const stream = new Stream.Writable();
+
+    // WriteStream.prototype._write 需要手动实现
+    stream._write = function (data, encoding, cb) {
+      console.log(data.toString());
+      // 控制速率, 一定的时间写入一次
+      setTimeout(() => {
+        cb()
+      }, 1000);
+    }
+
+    stream.on('finish', e => console.log('FINISH'))
+    stream.on('close', e => console.log('CLOSE'))
+    stream.on('drain', e => console.log('DRAIN'))
+
+    stream.write('锄禾日当午')
+    stream.write('汗滴禾下土')
+    stream.write('谁知盘中餐')
+    stream.write('粒粒皆辛苦')
+    stream.end('悯农')
+  }
+}
+
+main()
