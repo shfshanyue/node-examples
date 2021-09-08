3173c3bd1c82c68959f8ffb19308b42ea18fab88
shfshanyue <xianger94@gmail.com>
2021-09-08 23:45:45 +0800

    chore: http stream

--- server/hello/index.js
+++ server/hello/index.js
@@ -1,26 +1,41 @@
 const http = require('http')
+const fs = require('fs')
 
-const RUNID = 2
-const servers = [0]
+// 运行示例的编号
+const RUN_ID = 3
+const requestLiteners = [() => {}]
 
+// 示例一: hello, world
 {
-  const server = http.createServer((req, res) => {
+  const handleRequest = (req, res) => {
     // res.end 表示所有的 header 以及 body 发送完毕，Server 将标记该 Message 为 complete
     res.end('hello, world')
-  })
+  }
 
-  servers.push(server)
+  requestLiteners.push(handleRequest)
 }
 
+// 示例二: Transfer-Encoding
 {
   // 当调用了 res.write 及 res.end 时，报文将以 Transfer-Encoding: chunked 返回，与示例一不同
-  const server = http.createServer((req, res) => {
+  const handleRequest = (req, res) => {
     res.write('hello, world')
     res.end()
-  })
+  }
 
-  servers.push(server)
+  requestLiteners.push(handleRequest)
 }
 
-const server = servers[RUNID]
-server.listen(3000)
+// 示例三: stream
+{
+  const handleRequest = (req, res) => {
+    fs.createReadStream('../../package.json', {
+      // highWaterMark: 16
+    }).pipe(res)
+  }
+
+  requestLiteners.push(handleRequest)
+}
+
+const server = http.createServer(requestLiteners[RUN_ID])
+server.listen(3200)
