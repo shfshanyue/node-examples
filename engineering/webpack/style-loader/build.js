a1400f88bb3cb23fdc0f9202800d1545def6d3d2
shfshanyue <xianger94@gmail.com>
2021-10-14 14:57:31 +0800

    chore: Add mini-css-extract-plugin

--- engineering/webpack/style-loader/build.js
+++ engineering/webpack/style-loader/build.js
@@ -1,5 +1,7 @@
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
 
 function f1 () {
   return webpack({
@@ -13,12 +15,39 @@ function f1 () {
         }
       ]
     },
+    output: {
+      clean: true,
+      path: path.resolve(__dirname, 'dist/css')
+    },
     plugins: [
       new HtmlWebpackPlugin()
     ]
   })
 }
 
-f1().run(() => {
+function f2 () {
+  return webpack({
+    entry: './index.js',
+    mode: 'none',
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader']
+        }
+      ]
+    },
+    output: {
+      clean: true,
+      path: path.resolve(__dirname, 'dist/extract')
+    },
+    plugins: [
+      new HtmlWebpackPlugin(),
+      new MiniCssExtractPlugin()
+    ]
+  })
+}
+
+f2().run(() => {
 
 })
