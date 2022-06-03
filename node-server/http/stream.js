a45df894fde0457ce34414774d622fa7901bb300
shfshanyue <xianger94@gmail.com>
2022-06-03 09:23:13 +0800

    chore: htpp

--- node-server/http/stream.js
+++ node-server/http/stream.js
@@ -1,13 +1,18 @@
 const http = require('http')
 const sleep = (seconds) => new Promise(resolve => setTimeout(resolve, seconds))
 
+// 1. 在流式传输中，设置 content-length 的意义在哪里
+
 // Transfer-Encoding: chunked 是在何时进行配置的
 const server = http.createServer(async (req, res) => {
-  res.write('hello.') 
+  // 1. 设置 content-length 的意义在哪里
+  res.setHeader('content-length', 18)
+
+  res.write('hello, ') 
   await sleep(3000)
-  res.write('shanyue')
+  res.write('shanyue.')
   await sleep(3000)
-  res.end()
+  res.end('end')
 })
 
 server.listen(3000)
