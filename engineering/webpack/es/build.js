b3d20c3a966b99fa326f722fdfcc06ba3b67e07c
shfshanyue <xianger94@gmail.com>
2022-07-30 17:43:47 +0800

    chore: update

--- engineering/webpack/es/build.js
+++ engineering/webpack/es/build.js
@@ -2,8 +2,8 @@ const webpack = require('webpack')
 
 // `webpack-cli` 与 `webpack/cli` 调来调去，逻辑复杂，为了方便，直接使用其 node api 进行示例，方便调试
 
-// 1. webpack 的运行时代码是如何加载模块的
-// 1. [content-hash].js 中的 hash 是如何生成的
+// 1. 关于 ES 理解 webpack 的运行时代码
+// 2. ES Module 是如何转化为 CommonJS 的
 // 
 
 function f1 () {
@@ -13,26 +13,6 @@ function f1 () {
   })
 }
 
-function f2 () {
-  return webpack({
-    entry: './index.js',
-    mode: 'production',
-    output: {
-      filename: 'main.production.js'
-    }
-  })
-}
-
-function f3 () {
-  return webpack({
-    entry: './index.js',
-    mode: 'none',
-    output: {
-      filename: 'main.[contenthash].js'
-    }
-  })
-}
-
-f3().run((err, stat) => {
-  console.log(stat.toJson())
+f1().run((err, stat) => {
+  // console.log(stat.toJson())
 })
