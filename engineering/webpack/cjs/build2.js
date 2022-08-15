c01d2af726f56d44cf1e8dd1e482131e1e8f109e
shfshanyue <xianger94@gmail.com>
2022-08-15 12:59:29 +0800

    update

--- engineering/webpack/cjs/build2.js
+++ engineering/webpack/cjs/build2.js
@@ -2,13 +2,10 @@ const webpack = require('webpack')
 const path = require('path')
 const { stdout } = require('process')
 
-//
 // # 更好地查看日志
 // $ node build2.js  | jq -c -C ".[]" | less
-
-// webpack 的运行时代码分析
 function f1 () {
-  return webpack({
+  const compiler = webpack({
     entry: './index.js',
     mode: 'none',
     infrastructureLogging: {
@@ -16,18 +13,35 @@ function f1 () {
       stream: stdout
     }
   })
+
+  compiler.run((err, stat) => {
+    const logs = Array.from(stat.compilation.logging, ([hook, logs]) => {
+      return logs.map(r => {
+        return {
+          readableTime: new Date(r.time).toJSON(),
+          api: hook,
+          ...r,
+        }
+      })
+    }).flat()
+    console.log(JSON.stringify(logs))
+  })
 }
 
-// output.path 如何指定打包目录为 build 目录
-f1().run((err, stat) => {
-  const logs = Array.from(stat.compilation.logging, ([hook, logs]) => {
-    return logs.map(r => {
-      return {
-        readableTime: new Date(r.time).toJSON(),
-        api: hook,
-        ...r,
-      }
-    })
-  }).flat()
-  console.log(JSON.stringify(logs))
-})
+function f2 () {
+  const compiler = webpack({
+    entry: './index.js',
+    mode: 'none',
+    infrastructureLogging: {
+      debug: true,
+      stream: stdout
+    }
+  })
+
+  compiler.run((err, stat) => {
+    console.log(stat.toJson().time)
+    console.log(stat.endTime - stat.startTime)
+  })
+}
+
+f2()
