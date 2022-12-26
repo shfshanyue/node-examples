39865cd532d98671ec3028a9b571127e61688742
shfshanyue <xianger94@gmail.com>
2022-12-26 20:38:40 +0800

    chore: http/stream

--- node-server/http/stream.js
+++ node-server/http/stream.js
@@ -1,18 +1,29 @@
 const http = require('http')
-const sleep = (seconds) => new Promise(resolve => setTimeout(resolve, seconds))
 
-// 1. 在流式传输中，设置 content-length 的意义在哪里
+const sleep = (seconds) => new Promise(resolve => setTimeout(resolve, seconds))
 
 // Transfer-Encoding: chunked 是在何时进行配置的
 const server = http.createServer(async (req, res) => {
-  // 1. 设置 content-length 的意义在哪里
-  res.setHeader('content-length', 18)
 
-  res.write('hello, ') 
+  res.write(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <title>测试页面</title>
+    </head>
+    <body>`
+  )
   await sleep(3000)
-  res.write('shanyue.')
+  res.write('<h1>hello, shanyue</h1>')
   await sleep(3000)
-  res.end('end')
+  res.write('<div>这里是大段文字，将会延迟三秒才会正常渲染</div>')
+  await sleep(3000)
+  res.write('<div>这里又是大段文字，将会延迟三秒才会正常渲染</div>')
+  res.end(`
+    </body>
+    </html>
+  `)
 })
 
 server.listen(3000)
